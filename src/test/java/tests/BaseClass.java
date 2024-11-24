b95f4f9dbf2f9cc3a3898f06c3f5754fd19fe99f
asit05 <asitkaran137@gmail.com>
2024-11-24 16:34:13 +0530

    chrome options for ec2

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -10,6 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -24,30 +25,33 @@ public class BaseClass {
 	public static WebDriver driver;
 	public XSSFWorkbook wbook;
 	public XSSFSheet sheet;
-	
-	
+
 	private ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
 	public void SetDriver(WebDriver driver) {
 		threadLocalDriver.set(driver);
 	}
-	
+
 	public WebDriver GetDriver() {
 
 		return threadLocalDriver.get();
 
 	}
 
-	
-
 	@Parameters(/* "Browser" */)
 	@BeforeMethod(alwaysRun = true)
 	public void setup(/* String browser */) throws Throwable {
 
 		String browser = System.getProperty("Browser");
-	//	String browser = "chrome";
+		// String browser = "chrome";
 
-		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-using");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--headless");
+		options.addArguments("--ignore-ssl-errors=yes");
+		options.addArguments("--ignore-certificate-errors");
 
 		if (browser.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
@@ -72,22 +76,21 @@ public class BaseClass {
 	public void tearDown() {
 		GetDriver().quit();
 	}
-	
-	
+
 //	@BeforeTest(alwaysRun=true)
 	public void SetUpExcel() throws IOException {
-		
+
 		FileInputStream fis = new FileInputStream("exceldata.xlsx");
 		wbook = new XSSFWorkbook(fis);
 		sheet = wbook.getSheet("Sheet1");
-		
+
 	}
-	
+
 //	@AfterTest(alwaysRun=true)
 	public void CloseExcel() throws IOException {
-		
+
 		wbook.close();
-		
+
 	}
 
 }
